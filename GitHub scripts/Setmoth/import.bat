269c17d4c6840413d49c364ed38ce37be9667b9e
sethmoth1993 <lemaximetrudeau@hotmail.com>
2023-07-20 11:56:10 -0400

    First commit

--- GitHub scripts/Setmoth/import.bat
+++ GitHub scripts/Setmoth/import.bat
@@ -5,11 +5,11 @@ REM
 cd ..
 cd ..
 git add --all
-set /p commitDescription=Enter a brief description on what you modified
-SETLOCAL ENABLEDDELAYEDEXPANSION
-SET commitDescription=!commitDescription: =%" "!
-git commit -m %commitDescription%
+set /p commitDescription=Enter a brief description on what you modified:
+git commit -m "%commitDescription%"
 
 git pull origin master
 
 git switch setmoth
+
+PAUSE
