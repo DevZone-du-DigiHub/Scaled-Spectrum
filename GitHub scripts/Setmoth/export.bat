066ef537bcf55dc1b85396b62e6e7fadaf9931c2
sethmoth1993 <lemaximetrudeau@hotmail.com>
2023-07-20 11:58:46 -0400

    First commit

--- GitHub scripts/Setmoth/export.bat
+++ GitHub scripts/Setmoth/export.bat
@@ -3,11 +3,12 @@ REM
 
 cd ..
 cd ..
+
+git switch setmoth
 git add --all
 set /p commitDescription=Enter a brief description on what you modified:
 git commit -m "%commitDescription%"
 
 git pull origin master
 
-git switch setmoth
-git push origin master
+git push origin setmoth
--- GitHub scripts/Setmoth/import.bat
+++ GitHub scripts/Setmoth/import.bat
@@ -4,12 +4,13 @@ REM
 
 cd ..
 cd ..
+
+git switch setmoth
 git add --all
 set /p commitDescription=Enter a brief description on what you modified:
 git commit -m "%commitDescription%"
 
 git pull origin master
 
-git switch setmoth
 
 PAUSE
