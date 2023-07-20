9421932b2986619c60df63699332fdcdedb2b202
sethmoth1993 <lemaximetrudeau@hotmail.com>
2023-07-20 11:56:43 -0400

    Editing GitHub scripts

--- GitHub scripts/Setmoth/export.bat
+++ GitHub scripts/Setmoth/export.bat
@@ -1,14 +1,11 @@
 @echo off
 REM
 
-
 cd ..
 cd ..
 git add --all
 set /p commitDescription=Enter a brief description on what you modified:
-SETLOCAL ENABLEDDELAYEDEXPANSION
-SET commitDescription=!commitDescription: =%" "!
-git commit -m %commitDescription%
+git commit -m "%commitDescription%"
 
 git pull origin master
 
