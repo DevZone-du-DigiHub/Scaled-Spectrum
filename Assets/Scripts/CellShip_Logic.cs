fd1b0c5b3314c7a748e595cde2bd91950dc898fd
sethmoth1993 <lemaximetrudeau@hotmail.com>
2023-07-22 15:23:53 -0400

    Added UI elements + a bunch of uselessness in some scripts (sorry) + some changes i dont remember ( i need to write down things on the spot) j'ai pas modif tes script Jim

--- Assets/Scripts/CellShip_Logic.cs
+++ Assets/Scripts/CellShip_Logic.cs
@@ -5,7 +5,13 @@ public class CellShip_Logic : MonoBehaviour
 {
     [SerializeField] private Collider2D circleCollider;
     [SerializeField] private Collider2D capsuleCollider;
+    [SerializeField] private float snapDistance = 2f; // The distance between the player and the ship for SnapToMidPoint to work
+    [SerializeField] private GameObject snapIndicator; // Reference to the UI GameObject that shows the snap indicator
+    [SerializeField] private GameObject imageToActivate; // Reference to the GameObject containing the image to activate when inside the ship
+    [SerializeField] private float imageActivationDistance = 1.5f; // The distance from the center of the capsule collider to activate the image
     [SerializeField] private string highlightedAction;
+
+    private bool insideShipObject = false; // Flag to track if the player is inside the ship object
 
     private void Update()
     {
@@ -15,26 +21,19 @@ public class CellShip_Logic : MonoBehaviour
             {
                 if (!capsuleCollider.OverlapPoint(transform.position))
                 {
-                    // Snap player to the middle of the capsule collider if outside
-                    SnapToMidPoint();
-                    highlightedAction = "Snap to MidPoint";
+                    // Snap player to the middle of the capsule collider if outside and within snapDistance
+                    if (Vector2.Distance(transform.position, capsuleCollider.bounds.center) <= snapDistance)
+                    {
+                        SnapToMidPoint();
+                        highlightedAction = "Snap to MidPoint";
+                    }
                 }
-                
             }
-            else if (Gamepad.current.buttonEast.wasPressedThisFrame)
+            else if (Gamepad.current.buttonEast.wasPressedThisFrame && insideShipObject)
             {
-                if (!circleCollider.OverlapPoint(transform.position))
-                {
-                    // Snap player to the edge of the circle collider if outside
-                    SnapToEdge();
-                    highlightedAction = "Snap to Edge";
-                }
-                else
-                {
-                    // Teleport player outside the collider
-                    TeleportOutside();
-                    highlightedAction = "Teleport Outside";
-                }
+                // Perform action specific to the East button (TeleportOutside)
+                TeleportOutside();
+                highlightedAction = "Teleport Outside";
             }
             else if (Gamepad.current.buttonNorth.wasPressedThisFrame)
             {
@@ -49,11 +48,34 @@ public class CellShip_Logic : MonoBehaviour
             {
                 if (!capsuleCollider.OverlapPoint(transform.position))
                 {
-                    // Perform action specific to the B button
-                    PerformActionButtonB();
-                    highlightedAction = "Action B";
+                    // Perform action specific to the B button only if inside the ship and within activation distance
+                    if (insideShipObject && Vector2.Distance(transform.position, capsuleCollider.bounds.center) <= imageActivationDistance)
+                    {
+                        PerformActionButtonB();
+                        highlightedAction = "Action B";
+                    }
                 }
             }
+        }
+
+        // Show or hide the snap indicator based on the player's distance from the ship and not inside the snap indicator
+        if (snapIndicator != null && !circleCollider.OverlapPoint(transform.position))
+        {
+            snapIndicator.SetActive(Vector2.Distance(transform.position, capsuleCollider.bounds.center) <= snapDistance);
+        }
+        else if (snapIndicator != null)
+        {
+            snapIndicator.SetActive(false);
+        }
+
+        // Check if the player is inside the ship to activate the image
+        if (insideShipObject && imageToActivate != null && Vector2.Distance(transform.position, capsuleCollider.bounds.center) <= imageActivationDistance)
+        {
+            imageToActivate.SetActive(true);
+        }
+        else if (imageToActivate != null)
+        {
+            imageToActivate.SetActive(false);
         }
     }
 
@@ -126,5 +148,21 @@ public class CellShip_Logic : MonoBehaviour
         }
 
         return transform.position;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Ship")) // Assuming the ship object has a tag "Ship"
+        {
+            insideShipObject = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Ship")) // Assuming the ship object has a tag "Ship"
+        {
+            insideShipObject = false;
+        }
     }
 }
--- Assets/Scripts/StayCenter.cs
+++ Assets/Scripts/StayCenter.cs
@@ -3,7 +3,10 @@ using UnityEngine;
 public class StayCenter : MonoBehaviour
 {
     [SerializeField] private GameObject cellShip;
+    [SerializeField] private Canvas canvasToActivate;
     private SpriteRenderer spriteRenderer;
+
+    private bool isPlayerInside = false;
 
     private void Start()
     {
@@ -19,6 +22,37 @@ public class StayCenter : MonoBehaviour
 
             // Move the object to the center of the sprite
             transform.position = center;
+
+            if (isPlayerInside)
+            {
+                // Activate the canvasToActivate when the player is inside the cellShip
+                canvasToActivate.enabled = true;
+            }
+            else
+            {
+                // Deactivate the canvasToActivate when the player is outside the cellShip
+                canvasToActivate.enabled = false;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        // Assuming the player object has a tag "Player"
+        if (other.CompareTag("Player"))
+        {
+            // Set the flag to true when the player is inside the cellShip
+            isPlayerInside = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        // Assuming the player object has a tag "Player"
+        if (other.CompareTag("Player"))
+        {
+            // Set the flag to false when the player is outside the cellShip
+            isPlayerInside = false;
         }
     }
 }
