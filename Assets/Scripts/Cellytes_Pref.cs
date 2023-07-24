82706d66f35cf53d4d1bc3aa1ded82740ecc3fae
sethmoth1993 <lemaximetrudeau@hotmail.com>
2023-07-24 12:34:08 -0400

    new enemy ai (need work) new projectil few parameters changes

--- Assets/Scripts/Cellytes_Pref.cs
+++ Assets/Scripts/Cellytes_Pref.cs
@@ -6,9 +6,12 @@ public class Cellytes_Pref : MonoBehaviour
     [SerializeField] private bool spiralMovement = false; // Toggle for spiral movement
     [SerializeField] private float spiralSpeed = 2f; // Speed of spiral rotation
     [SerializeField] private int maximumQuantity = 10; // Maximum allowed quantity of projectiles
+    [SerializeField] private float spawnCooldown = 2f; // Cooldown duration in seconds
 
     private Rigidbody2D rb;
     private float angle = 0f;
+    private float cooldownTimer = 0f;
+    private bool launched = false; // Flag to indicate if the projectile has been launched
 
     private static int currentQuantity = 0; // Current quantity of projectiles
 
@@ -25,7 +28,12 @@ public class Cellytes_Pref : MonoBehaviour
 
     private void Update()
     {
-        if (spiralMovement)
+        if (cooldownTimer > 0f)
+        {
+            cooldownTimer -= Time.deltaTime;
+        }
+
+        if (spiralMovement && !launched) // Only apply spiral movement if not launched
         {
             angle += spiralSpeed * Time.deltaTime;
             transform.rotation = Quaternion.Euler(0f, 0f, angle);
@@ -55,5 +63,23 @@ public class Cellytes_Pref : MonoBehaviour
     private void SelfDestruct()
     {
         Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        // Check if the projectile collided with something other than the player
+        if (!other.CompareTag("Player") && launched) // Only destroy if launched and not colliding with the player
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void Launch()
+    {
+        if (cooldownTimer <= 0f)
+        {
+            launched = true; // Called when you want to launch the projectile
+            cooldownTimer = spawnCooldown; // Set the cooldown timer
+        }
     }
 }
